9eca04f9c66a4c0bffbbcac8792f5f9ef46333e4
Lucas Nunes <lucasmnunesk@gmail.com>
2017-04-16 02:53:26 -0300

    Module 9 Travis CI

--- BuildScripts/testSetup.js
+++ BuildScripts/testSetup.js
@@ -2,5 +2,6 @@
 
 //Register babel to transpile before our tests run.
 require('babel-register')();
+
 //Disable webpack features that Mocha doesn't understand
 require.extensions['.css'] = function() {};
